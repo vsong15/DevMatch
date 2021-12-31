d1a745077dc1007030c99842b36f909d190c0fd2
Vincent Song <dela.vincent.15@gmail.com>
2021-12-31 18:19:16 +0000

    Added server side code for creating subscriptions for pro users

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Extend default Devise gem behavior so that users signing up with the pro
+  # account (plan ID 2) save with a special stripe subscription function.
+  # Otherwise, Devise signs up user as usual.
   def create
     super do |resource|
       if params[:plan]
